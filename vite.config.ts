c1cb4ce932703c53a76fec2267ed82046758b4bf
Denis Ergin <denis25657@hotmail.de>
2025-10-13 15:02:40 +0200

    fix: final build fixes to make it work in a linked project

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,32 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import { resolve } from 'node:path'
+import fs from 'node:fs';
+import postcss from 'postcss';
+import cssnano from 'cssnano';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    VueDevTools(),
+    VueDevTools(), {
+      name: 'minify-extra-css',
+      apply: 'build',
+      async closeBundle() {
+        const input = resolve("src/assets/vars.css");
+        const output = resolve("dist/vars.css");
+
+        const css = fs.readFileSync(input, 'utf8');
+
+        const result = await postcss([cssnano()]).process(css, {
+          from: input,
+          to: output,
+        });
+
+        fs.writeFileSync(output, result.css, 'utf8');
+      }
+    }
   ],
   build: {
     lib: {
